0b21d80153de0e5232692b1be638fc9a2c93a5e1
Samuele Salica <reiz@MBP-di-Samuele.homenet.telecomitalia.it>
2024-07-14 12:45:24 +0200

    implementata funzione email

--- server.js
+++ server.js
@@ -3,13 +3,29 @@ const path = require("path");
 const app = express();
 
 // Middleware per servire i file statici
-app.use("/css", express.static(path.join(__dirname, "assets", "css")));
-app.use("/js", express.static(path.join(__dirname, "assets", "js")));
-app.use("/img", express.static(path.join(__dirname, "assets", "img")));
-app.use("/scss", express.static(path.join(__dirname, "assets", "scss")));
+app.use(express.static(path.join(__dirname, "./assets/css/styles.css")));
+app.use(express.static(path.join(__dirname, "./assets/js/main.js")));
+app.use(express.static(path.join(__dirname, "./assets/img/about.jpg")));
+app.use(express.static(path.join(__dirname, "./assets/img/perfil.png")));
+app.use(express.static(path.join(__dirname, "./assets/img/work1.jpg")));
+app.use(express.static(path.join(__dirname, "./assets/img/work2.jpg")));
+app.use(express.static(path.join(__dirname, "./assets/img/work3.jpg")));
+app.use(express.static(path.join(__dirname, "./assets/img/work4.jpg")));
+app.use(express.static(path.join(__dirname, "./assets/img/work5.jpg")));
+app.use(express.static(path.join(__dirname, "./assets/img/work6.jpg")));
+app.use(express.static(path.join(__dirname, "assets/scss/styles.scss")));
+app.use(express.static(path.join(__dirname, "/about.jpg")));
+app.use(express.static(path.join(__dirname, "/perfil.png")));
+app.use(express.static(path.join(__dirname, "/preview.png")));
+app.use(express.static(path.join(__dirname, "/work1.jpg")));
+app.use(express.static(path.join(__dirname, "/work2.jpg")));
+app.use(express.static(path.join(__dirname, "/work3.jpg")));
+app.use(express.static(path.join(__dirname, "/work4.jpg")));
+app.use(express.static(path.join(__dirname, "/work5.jpg")));
+app.use(express.static(path.join(__dirname, "/work6.jpg")));
 // Route per la homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 // Avvio del server
